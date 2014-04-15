7f8d981a2881f7b5cbf37e74a03fd484843a7412
Daniel Neis Araujo <danielneis@gmail.com>
2014-04-15 12:50:19 -0300

    Finalizando interface 1.0

--- js/interface.js
+++ js/interface.js
@@ -1,22 +1,21 @@
 $(function(){
 
     // labels dos sliders
-    $('#esportesLivresFreqLabel').text($('#esportesLivresFreq').val());
+    $('#esportesLivresFreqLabel').text($('#esportesLivresFreq').val() + ' dias por semana');
     $("#esportesLivresFreq").on('change', function(evnt) {
-        $("#esportesLivresFreqLabel").text($('#esportesLivresFreq').val());
+        $("#esportesLivresFreqLabel").text($('#esportesLivresFreq').val() + ' dias por semana');
     });
 
-    $('#esportesColFreqLabel').text($('#esportesColFreq').val());
+    $('#esportesColFreqLabel').text($('#esportesColFreq').val() + ' dias por semana');
     $("#esportesColFreq").on('change', function(evnt) {
-        $("#esportesColFreqLabel").text($('#esportesColFreq').val());
+        $("#esportesColFreqLabel").text($('#esportesColFreq').val() + ' dias por semana');
     });
 
-    $('#esportesAcadFreqLabel').text($('#esportesAcadFreq').val());
+    $('#esportesAcadFreqLabel').text($('#esportesAcadFreq').val() + ' dias por semana');
     $("#esportesAcadFreq").on('change', function(evnt) {
-        $("#esportesAcadFreqLabel").text($('#esportesAcadFreq').val());
+        $("#esportesAcadFreqLabel").text($('#esportesAcadFreq').val() + ' dias por semana');
     });
 
 
     $('#navbar').affix();
-
 });
