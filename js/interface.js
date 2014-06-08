f43d50d792afbe785445118d4627c1044dc8c72d
Daniel Neis Araujo <danielneis@gmail.com>
2014-06-08 18:02:43 -0300

    usabilidade

--- js/interface.js
+++ js/interface.js
@@ -16,6 +16,35 @@ $(function(){
         $("#esportesAcadFreqLabel").text($('#esportesAcadFreq').val() + ' dias por semana');
     });
 
+    $('#idadeLabel').text($('#idade').val() + ' anos completos');
+    $("#idade").on('change', function(evnt) {
+        $("#idadeLabel").text($('#idade').val() + '  anos completos');
+    });
+
+    $('#pesoLabel').text($('#peso').val() + ' kilogramas');
+    $("#peso").on('change', function(evnt) {
+        $("#pesoLabel").text($('#peso').val() + ' kilogramas');
+    });
+
+    $('#alturaLabel').text($('#altura').val() + ' centímetros');
+    $("#altura").on('change', function(evnt) {
+        $("#alturaLabel").text($('#altura').val() + ' centímeros');
+    });
+
+    $('#circunferencia_abdominalLabel').text($('#circunferencia_abdominal').val() + ' centímetros');
+    $("#circunferencia_abdominal").on('change', function(evnt) {
+        $("#circunferencia_abdominalLabel").text($('#circunferencia_abdominal').val() + ' centímeros');
+    });
+
+    $('#pressao_sistolicaLabel').text($('#pressao_sistolica').val() + ' mmHg');
+    $("#pressao_sistolica").on('change', function(evnt) {
+        $("#pressao_sistolicaLabel").text($('#pressao_sistolica').val() + ' mmHg');
+    });
+
+    $('#pressao_diastolicaLabel').text($('#pressao_diastolica').val() + ' mmHg');
+    $("#pressao_diastolica").on('change', function(evnt) {
+        $("#pressao_diastolicaLabel").text($('#pressao_diastolica').val() + ' mmHg');
+    });
 
     $('#navbar').affix({offset: {top: 55}});
 });
